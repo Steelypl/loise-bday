c7690f4d8fc896fb0436766c84e9525d12a58a1f
sebastiancorcino <sebastiancorcino@gmail.com>
2025-06-01 20:51:57 -0400

    fix fr

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import BirthdayPage from "./pages/BirthdayPage";
 import SurprisePage from "./pages/SurprisePage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<BirthdayPage />} />
         <Route path="/surprise" element={<SurprisePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
