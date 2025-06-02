c5120b107cf6349390e498fdf5cd33ad3171a650
sebastiancorcino <sebastiancorcino@gmail.com>
2025-06-01 20:38:04 -0400

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import BirthdayPage from "./pages/BirthdayPage";
 import SurprisePage from "./pages/SurprisePage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<BirthdayPage />} />
-        <Route path="/surprise" element={<SurprisePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<BirthdayPage />} />
+      <Route path="/surprise" element={<SurprisePage />} />
+    </Routes>
   );
 }
 
