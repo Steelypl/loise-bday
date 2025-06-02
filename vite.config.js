3909c5aab5247713f9e037b91b6b226221f6ceed
sebastiancorcino <sebastiancorcino@gmail.com>
2025-06-01 20:42:13 -0400

    fix fr

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/loise-bday",
+  base: "/princess-bday",
   plugins: [react(), tailwindcss()],
 });
